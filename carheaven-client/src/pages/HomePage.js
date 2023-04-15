2d9eb3fe43feb527100adb4a4e641e691974161c
drose123456 <acg_danial@hotmail.com>
2023-04-15 21:32:51 +0100

    added comments for home page

--- carheaven-client/src/pages/HomePage.js
+++ carheaven-client/src/pages/HomePage.js
@@ -6,10 +6,16 @@ const API_URL = "http://localhost:3000";
 
 function HomePage(){
     const [cars, setCars] = useState([]);
+    /*useState function initializing cars variable
+    using empty array */
 
+    /*useEffect hook used to fetch data from API*/
     useEffect(() => {
         axios.get(`${API_URL}/api/cars`)
-        .then((data) => {
+        /*method called to make GET req to API endpoint*/
+        .then((data) => { 
+        /*then method of axios promise used to update the
+        state of the variable with the res data from API*/
           console.log(data);
           setCars(data?.data);
         })
@@ -17,9 +23,12 @@ function HomePage(){
       }, []);
 
     return (
+        /*MAP METHOD - used on cars array to loop through each and pass
+        properties as object to carCard component*/
         <div className="HomePage">
             { cars.map((car) => <CarCard key={car._id} {...car} /> )}
         </div>
+        /*carCard recieves unique key prop based on cars id property*/
     );
 }
 
