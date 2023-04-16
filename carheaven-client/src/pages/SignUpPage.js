e013112e5f65a9a41c10045093cfb31bee55ff2a
drose123456 <acg_danial@hotmail.com>
2023-04-16 21:36:59 +0100

    sign up page implemented

--- carheaven-client/src/pages/SignUpPage.js
+++ carheaven-client/src/pages/SignUpPage.js
@@ -1,40 +1,34 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext } from "./../context/auth.context";
 
 const API_URL = "http://localhost:3001";
 
-function LoginPage(props) {
+function SignUpPage(props) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
     const [errorMessage, setErrorMessage] = useState(undefined);
     
-    // allowing component to redirect user to different page after successful login
+    // allowing component to redirect user to different page after successful sign up
     const navigate = useNavigate();
-    //importint AuthContext with contains authentication
-    // allows auth methods to be accessed through hook
-    const { storeToken, authenticateUser } = useContext(AuthContext);
+    
 
     // function updates state variable in response to user input
     const handleEmail = (e) => setEmail(e.target.value);
     const handlePassword = (e) => setPassword(e.target.value);
+    const handleName = (e) => setName(e.target.value);
 
-    const handleLoginSubmit = (e) => { // trigerrred when user submits login form
+    const handleSignUpSubmit = (e) => { // trigerrred when user submits sign up form
         e.preventDefault();
         //Creating an object representing the request body
-        const requestBody = { email, password };
+        const requestBody = { email, password, name };
 
         //sending POST req to API 
-        axios.post(`${API_URL}/auth/login`, requestBody)
+        axios.post(`${API_URL}/auth/signup`, requestBody)
         .then((response) => {
-            console.log("JWT token", response.data.authToken);
-            // if req is succesful, auth token is saved in browsers local storage
-            storeToken(response.data.authToken);
-            // updates isAuthenticated state to true
-            authenticateUser();
             // redirects user to the home page
-            navigate("/");
+            navigate("/login");
         })
         // if req fails, error message is set using hook
         .catch((error) => {
@@ -44,24 +38,26 @@ function LoginPage(props) {
 
     };
     return (
-        <div className="LoginPage">
-            <h1>Login</h1>
-            <form onSubmit={handleLoginSubmit}>
+        <div className="SignUpPage">
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSignUpSubmit}>
+            <label>Name: </label>
+            <input type="text" name="name" value={name} onChange={handleName}/>
             <label>Email:</label>
             <input type="email" name="email" value={email} onChange={handleEmail} />
 
             <label>Password:</label>
             <input type="password" name="password" value={password} onChange={handlePassword} />
-                
-            <button type="submit">Login</button>
+    
+            <button type="submit">Sign Up</button>
             </form>
             { errorMessage && <p className="error-message">{errorMessage}</p> }
 
-            <p>Don't have an account yet?</p>
-            <Link to={"/signup"}> Sign Up</Link>
+            <p>Already have an account created?</p>
+            <Link to={"/login"}> Login</Link>
         </div>
     )
 
 }
 
-export default LoginPage;
+export default SignUpPage;
