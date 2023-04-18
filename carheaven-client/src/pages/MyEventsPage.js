536b95391d025228da39436fec2081c9774b15ed
drose123456 <acg_danial@hotmail.com>
2023-04-18 08:18:32 +0100

    myeventspage no route matched location fixed

--- carheaven-client/src/pages/MyEventsPage.js
+++ carheaven-client/src/pages/MyEventsPage.js
@@ -2,28 +2,33 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 import EventCard from "../components/EventCard";
 
+import AddEvent from "../components/AddEvent";
+
 const API_URL = "http://localhost:3000";
 
 function MyEventsPage(){
     const [events, setEvents] = useState([]);
 
-    useEffect(() => {
-        const storedToken = localStorage.getItem('authToken');
+    const getMyEvents = () => {
+        const storedToken = localStorage.getItem("authToken");
         const storeUserId = localStorage.getItem("userId");
+        
         axios
         .get(
-            `${API_URL}/api/events/eventId/${storeUserId}`,
+            `${API_URL}/api/events/myEvents/${storeUserId}`,
             { headers: { Authorization: `Bearer ${storedToken}` } }    
         )
-        .then((data) => {
-            console.log(data);
-            setEvents(data?.data);
-        })
-        .catch((error) => {console.log(error)});
-    }, [])
+        .then((response) => setEvents(response.data))
+        .catch((error) => console.log(error));
+    };
 
+    useEffect(() => {
+        getMyEvents();
+    }, []);
+ 
     return (
         <div className="MyEventsPage">
+                <AddEvent refreshEvents={getMyEvents}/>
             { events.map((event) => <EventCard key={event._id} {...event} /> )}
         </div>
     )
