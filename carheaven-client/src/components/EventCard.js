9a185750ccc14ca156466ba5a7ca5b3934548f9b
drose123456 <acg_danial@hotmail.com>
2023-04-20 16:13:10 +0100

    removed console log

--- carheaven-client/src/components/EventCard.js
+++ carheaven-client/src/components/EventCard.js
@@ -2,8 +2,6 @@ import { Link } from "react-router-dom";
 
 function EventCard ({ title, description, imageUrl, location, creator, status, _id}) {
 
-    console.log(creator);
-
 return (
     <div className="EventCard card">
         <Link to = {`/events/${_id}`}>
